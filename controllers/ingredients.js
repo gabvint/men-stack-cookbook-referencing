d651ede7f0f830d645a44e7d51f0efd409a87c49
gabtongol <geybtongol@gmail.com>
2024-08-01 19:44:35 -0400

    controller and middleware added

---
+++ controllers/ingredients.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js')
+const Ingredients = require('../models/ingredients.js')
+
+module.exports = router;
