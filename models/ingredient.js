92fa75969ee098d06367ec6267a9830f958dcbc7
gabtongol <geybtongol@gmail.com>
2024-08-01 19:33:48 -0400

    recipe and ingredient schema

---
+++ models/ingredient.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ingredientsSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+    }
+
+});
+
+const Ingredient = mongoose.model('Ingredient', ingredientsSchema);
+module.exports = Ingredient;
