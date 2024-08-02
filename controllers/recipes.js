0283374f90039e0be0a5425e8f8fb4fc142e5d91
gabtongol <geybtongol@gmail.com>
2024-08-01 21:29:57 -0400

    edit, update and delete working but without instruction

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -8,7 +8,8 @@ const Recipe = require('../models/recipe.js')
 
 router.get('/', async (req, res) => {
     try {   
-        const allRecipes = await Recipe.find({}).populate('owner')
+        // finds the recipes of the user
+        const allRecipes = await Recipe.find({ owner: req.session.user._id})
     
         res.render('recipes/index.ejs', {
             recipes: allRecipes,
@@ -19,6 +20,7 @@ router.get('/', async (req, res) => {
     }
 })
 
+// add new recipe
 router.get('/new', async (req, res) => {
     try {
         res.render('recipes/new.ejs');
@@ -28,6 +30,7 @@ router.get('/new', async (req, res) => {
     }
 })
 
+// add new recipe in database
 router.post('/', async (req, res) => {
     try {
         const newRecipe = new Recipe(req.body)
@@ -42,6 +45,59 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/:recipeId', async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.recipeId)
+        res.render('recipes/show.ejs', {
+            recipe: recipe,
+        })
+    } catch (error) {
+        console.log(error)
+        res.redirect('/');
+    }
+
+})
 
 
+router.delete('/:recipeId', async (req, res) => {
+    try {
+       await Recipe.findByIdAndDelete(req.params.recipeId)
+
+       res.redirect('/recipes')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/');
+    }
+})
+
+router.get('/:recipeId/edit', async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.recipeId)
+
+        res.render('recipes/edit.ejs', {
+            recipe: recipe,
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.redirect('/');
+    }
+})
+
+router.put('/:recipeId', async (req, res) => {
+    try {
+        const currentRecipe = await Recipe.findById(req.params.recipeId)
+
+        if (currentRecipe.owner.equals(req.session.user._id)){
+            await currentRecipe.updateOne(req.body)
+            res.redirect(`/recipes/${req.params.recipeId}`)
+        }else{
+            res.send('You dont have permission to edit this')
+        }
+    } catch (error) {
+        console.log(error)
+        res.redirect('/');
+    }
+
+})
 module.exports = router;
